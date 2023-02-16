74c64a3f554c8d1f3b1364cc8ecfa18d1df3f80b
N01452464 <90890132+N01452464@users.noreply.github.com>
2023-02-15 19:34:16 -0500

    Add files via upload

---
+++ database/data.js
@@ -0,0 +1,32 @@
+export default[
+    {
+        "_id": "63ed510979e29eca78ccac06",
+        "question_id": "1",
+        "questions": "do you have a Comprehensive (internal) Business plan?",
+        "answer": "\"\""
+    },
+    {
+        "_id": "63ed520379e29eca78ccac07",
+        "question_id": "2",
+        "questions": "does your business plan contain all of the following elements?",
+        "answer": "\"\""
+    },
+    {
+        "_id": "63ed52cf79e29eca78ccac08",
+        "question_id": "3",
+        "questions": "Do you have a NAICS Code?",
+        "answer": "\"\""
+    },
+    {
+        "_id": "63ed538779e29eca78ccac09",
+        "question_id": "4",
+        "questions": "Do you know your Industry's History?",
+        "answer": "\"\""
+    },
+    {
+        "_id": "63ed541479e29eca78ccac0a",
+        "question_id": "5",
+        "questions": "Do you know your Industry's Current Situation?",
+        "answer": "\"\""
+      }
+]
