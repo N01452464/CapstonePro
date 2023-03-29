a78cb79d3bff6e4e3ea1cfbbe2788b03b154b461
N01452464 <90890132+N01452464@users.noreply.github.com>
2023-03-29 16:05:06 -0400

    Add files via upload

---
+++ database/mk.js
@@ -0,0 +1,118 @@
+export default[
+    {
+         
+        question_id: "3",
+         question: "Have you Identified your overall Market?",
+         options:['Yes','No'],
+         Answer_type: "radio",
+         yesQuestionIndex:"4",
+         noQuestionIndex:"4",
+         category_type: "Market Analysis"
+       },{
+         
+         question_id: "4",
+         question: "Do you know your Market-level Metrics?",
+         options:['Yes','No'],
+         Answer_type: "radio",
+         yesQuestionIndex:"5",
+         noQuestionIndex:"5",
+         category_type: "Market Analysis"
+       },{
+         
+         question_id: "5",
+         question: "Have you Segmented your Market Adequately?",
+         options:['Yes','No'],
+         Answer_type: "radio",
+         yesQuestionIndex:"6",
+         noQuestionIndex:"6",
+         category_type: "Market Analysis"
+       },{
+         
+         question_id: "6",
+         question: "Do you know your Market's Demographics?",
+         options:['Yes','No'],
+         Answer_type: "radio",
+         yesQuestionIndex:"7",
+         noQuestionIndex:"7",
+         category_type: "Market Analysis",
+         
+       },{
+         question_id: "7",
+         question: "Do you know your Market's Psychographics?",
+         options:['Yes','No'],
+         Answer_type: "radio",
+         yesQuestionIndex:"8",
+         noQuestionIndex:"8",
+         category_type: "Market Analysis"
+       },
+       {
+        question_id: "8",
+        question: "Do you know your Market's Cohorts?",
+        options:['Yes','No'],
+        Answer_type: "radio",
+        yesQuestionIndex:"9",
+        noQuestionIndex:"9",
+        category_type: "Market Analysis"
+      },
+      {
+        question_id: "9",
+        question: "Do you know your Market's Growth Potential?",
+        options:['Yes','No'],
+        Answer_type: "radio",
+        yesQuestionIndex:"10",
+        noQuestionIndex:"10",
+        category_type: "Market Analysis"
+      },
+      {
+        question_id: "10",
+        question: "Do you know your Market's Attractiveness?",
+        options:['Yes','No'],
+        Answer_type: "radio",
+        yesQuestionIndex:"11",
+        noQuestionIndex:"11",
+        category_type: "Market Analysis"
+      },
+      {
+        question_id: "11",
+        question: "Do you know your Market's Outlook?",
+        options:['Yes','No'],
+        Answer_type: "radio",
+        yesQuestionIndex:"12",
+        noQuestionIndex:"12",
+        category_type: "Market Analysis"
+      },{
+        question_id: "12",
+        question: "Do you know your Market's Wants/Needs?",
+        options:['Yes','No'],
+        Answer_type: "radio",
+        yesQuestionIndex:"13",
+        noQuestionIndex:"13",
+        category_type: "Market Analysis"
+      },{
+        question_id: "13",
+        question: "Have you performed a Market Gap Analysis?",
+        options:['Yes','No'],
+        Answer_type: "radio",
+        yesQuestionIndex:"14",
+        noQuestionIndex:"14",
+        category_type: "Market Analysis"
+      },{
+        question_id: "14",
+        question: "Have you Spotted Meaningful Opportunities at the Market Level?",
+        options:['Yes','No'],
+        Answer_type: "radio",
+        yesQuestionIndex:"15",
+        noQuestionIndex:"15",
+        category_type: "Market Analysis"
+      },{
+        question_id: "15",
+        question: "Do you have an Executive Summary of the above?",
+        options:['Yes','No'],
+        Answer_type: "radio",
+        yesQuestionIndex:"16",
+        noQuestionIndex:"16",
+        category_type: "Market Analysis"
+      },
+ ]
+ 
+ export const manswers = [0,0,0,0,0,0]
