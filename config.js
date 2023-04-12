98428d9c359a379b160e2573c9a9b9dd2ca0c4ff
N01452464 <90890132+N01452464@users.noreply.github.com>
2023-04-12 16:00:13 -0400

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-export const PORT = 5000;
-export const database = 'mongodb+srv://guddan:Katariya25@cluster0.gfdhdra.mongodb.net/Project';
-export const secret = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFiY0BnbWFpbC5jb20iLCJpYXQiOjE2ODAxMjI3NzF9.wFAgi6MkbA6-F6NmPH8MWVtR_0eQNDYrJMqa-UpX4ok';
